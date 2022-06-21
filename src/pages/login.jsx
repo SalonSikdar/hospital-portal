94691c8efee8b40c22f3d34a59f7b99c06a619ad
Saloni Sikdar <salonisikdar@gmail.com>
2022-06-21 17:07:41 +0530

    added dummy comparison n input handlers to email n password

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
+import React, {useState } from "react";
 import "../styles/login.css";
 import "bootswatch/dist/lux/bootstrap.css";
+import _ from 'lodash';
 
 function Login() {
+  let [password, setPassword] = useState("");
+  let [email, setEmail] = useState("");
+
+  const handleSubmit = () => {
+    console.log('Submit clicked');
+    //encode password email here
+    //call API to verify password
+    //on success response show alert
+    if(_.isEmpty(email))
+    alert('Email is empty');
+    if(_.isEmpty(password))
+    alert('Password is Empty');
+    alert("Login Successful");
+  };
+
   return (
     <div className="outer-div">
       <div className="login-box">
         <h1>Login/Register</h1>
-        <div className="form-group">
-          <label for="exampleInputEmail1" className="form-label mt-4">
+        <div className="form-group" onSubmit={handleSubmit}>
+          <label className="form-label mt-4">
             Email address
           </label>
           <input
@@ -17,13 +33,14 @@ function Login() {
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <small id="emailHelp" className="form-text text-muted">
             We'll never share your email with anyone else.
           </small>
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1" className="form-label mt-4">
+          <label  className="form-label mt-4">
             Password
           </label>
           <input
@@ -31,10 +48,17 @@ function Login() {
             className="form-control"
             id="exampleInputPassword1"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <br/>
-        <button type="button" className="btn btn-primary">Login</button>
+        <br />
+        <button
+          type="submit"
+          className="btn btn-primary"
+          onClick={()=>handleSubmit()}
+        >
+          Login
+        </button>
       </div>
     </div>
   );
